b02a1e5d3f257be2744bbbbdf71f073d7599b58f
davi <davifernadesr@gmail.com>
2023-10-03 09:31:02 -0300

    feat: criação da tabela Compra_livro, assim com a criação de sua trigger, para adicionar livrod automaticamente na tabela livros

---
+++ compra_livro.sql
@@ -0,0 +1,42 @@
+create database Biblioteca
+
+create table Compra_livro(
+
+    id int PRIMARY KEY IDENTITY(1,1),
+    nome varchar(50) not null UNIQUE,
+    autor VARCHAR(50) not null,
+    paginas int not null,
+    quantidade int not null,
+    preco DECIMAL(10,2) not null,
+    data DATETIME not NULL
+
+);
+
+--teste
+
+INSERT into Compra_livro(nome, autor, paginas,quantidade,preco,data)values('Percy Jackson e o ladrão de raios', 'Rick Riordan', 265, 5 ,30.99, GETDATE())
+
+SELECT * from Compra_livro;
+
+select * from Livros;
+
+GO
+
+create TRIGGER adiciona_livro
+on Compra_livro
+for insert 
+   as BEGIN
+
+   declare
+   @nome VARCHAR(50),
+   @autor VARCHAR(50),
+   @paginas int,
+   @quantidade int
+
+   SELECT  @nome = nome, @autor = autor,@paginas = paginas ,@quantidade = quantidade from inserted
+
+   insert into Livros(nome, autor, paginas, quantidade) VALUES(@nome, @autor,@paginas,@quantidade)
+
+   END
+
+GO
