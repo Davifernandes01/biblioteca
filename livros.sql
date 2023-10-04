8da429565c1306a12cfceaa337fce8996fd99b9d
davi <davifernadesr@gmail.com>
2023-10-04 09:47:41 -0300

    feat: criação da trigger para avisar quando um livro chega na quantidade zero

--- livros.sql
+++ livros.sql
@@ -9,4 +9,23 @@ create table Livros(
   
 );
 
+--criação da trigger para avisar que a quantidade do livro chegou a zero
+go
+create TRIGGER verificaQuantidade
+on Livros
+after INSERT, UPDATE
+as 
+    BEGIN
+        DECLARE
+        @quantidade int,
+        @id int;
 
+        SELECT @quantidade = quantidade, @id = id from inserted;
+
+        if @quantidade = 0
+            BEGIN
+                RAISERROR('quantidade igual a zero! favor comprar mais livros', 14,1)
+            end;
+
+END
+go
