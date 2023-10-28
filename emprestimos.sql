42fcf72f7300335bc649a44bfe0e18c385091694
davi <davifernadesr@gmail.com>
2023-10-28 00:36:00 -0300

    trigger para adicionar o emprestimos na multa depois de 21 dias, feito parcialmente

--- emprestimos.sql
+++ emprestimos.sql
@@ -16,6 +16,7 @@ add CONSTRAINT ck_quantidade_limite
 check (quantidade >=1 and quantidade <=5);
 
 
+
 --criação das fks
 
 alter table Emprestimos
@@ -24,6 +25,8 @@ add CONSTRAINT fk_emprestimo_livro FOREIGN KEY (id_livro) REFERENCES Livros(id);
 alter table Emprestimos
 add CONSTRAINT fk_emprestimo_cliente FOREIGN key (id_cliente) REFERENCES Clientes(id);
 
+
+--testes
 
 
 --trigger para diminuir a quantidade de livros na tabela livros
@@ -60,4 +63,39 @@ END
 go
 
 
-drop TRIGGER diminui_quantidade_de_livro;
+--criação da trigger para colocar na tabela multa os clientes que não devolveram os livros ate a data estipulada.
+GO
+create trigger multa_emprestimos
+on Emprestimos
+after insert
+as BEGIN
+  DECLARE
+        @dataDevolver DATETIME,  
+        @id int,
+        @isdevolveu bit,
+
+        @dataDAQUI21DIAS DATETIME
+        set @dataDAQUI21DIAS = DATEADD(day,21,GETDATE())
+
+        --tabela emprestimo
+
+      select  @dataDevolver = dataDevolver, @id = id, @isdevolveu = isDevolveu from inserted;
+
+    --caso a @datadevolver seja igual a data atual, o  @isdevolveu passará a ser 0
+      update Emprestimos
+      set isDevolveu = 0
+      from Emprestimos
+      where @dataDAQUI21DIAS = @dataDevolver;
+
+
+--inserindo dados na tabela multas caso isdevolveu seja igual a 0
+  if @isdevolveu = 0
+    BEGIN
+     insert into Multas (valor, id_emprestimos) VALUEs(20.00, @id);
+    END
+            
+END
+go
+
+
+drop trigger multa_emprestimos
